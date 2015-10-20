debeca7fc9ad549f0a7aff5b4e6bbb413bb7e12b
weliton-bastos <prof.weliton@gmail.com>
2015-10-20 10:36:24 -0200

    pagina 24

--- TestGit3.java
+++ TestGit3.java
@@ -3,6 +3,7 @@ public class TestGit {
 	public static void main (String[] args) {
 
 		System.out.println("Exemplo de teste do uso do git 33333");
+System.out.println("mais uma linha p24");
 	}
 
 }
